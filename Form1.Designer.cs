fbc23181ce4ece26cc3d0f3e9466f4d19d44187a
WIN_NT\x16j057 <x16j057@chiba-fjb.ac.jp>
2017-06-02 11:54:35 +0900

    ゲームだよ

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,100 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(315, 283);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(48, 48);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(596, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(61, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "上";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.button1_KeyPress);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(523, 245);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(58, 48);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "左";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(637, 230);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 38);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "右";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(600, 283);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(56, 47);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "した";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 332);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,47 @@ namespace Game
 {
     public partial class Form1 : Form
     {
+        int speed = 10;
+        int movex;
+        int movey;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            movey = -speed;
+            movex = 0;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            movey = speed;
+            movex = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            movex = -speed;
+            movey = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            movex = speed;
+            movey = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left += movex;
+            pictureBox1.Top += movey;
+        }
+
+        private void button1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            pictureBox1.Top -= speed;
         }
     }
 }
